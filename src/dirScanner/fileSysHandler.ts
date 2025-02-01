22d2c58c0e84aa7a69e0a49287cb8a6b09f90f84
EmiRoberti77 <emi@datamotion.ai>
2025-02-01 14:53:43 +0000

    emit error if exception is cought during file scanning

--- src/dirScanner/fileSysHandler.ts
+++ src/dirScanner/fileSysHandler.ts
@@ -12,40 +12,37 @@ export class FileSysScanHandler extends EventEmitter {
 
   public async scan(dirPath: string = this.rootDir): Promise<void> {
     const now = Date.now();
-    const dir = await opendir(this.rootDir);
-    for await (const dirent of dir) {
-      const filePath = path.join(this.rootDir, dirent.name);
-      const fileStats = await stat(filePath);
-      const ageInSeconds = Math.floor((Date.now() - fileStats.mtimeMs) / 1000);
-      const ageInDays = Math.floor(ageInSeconds / 86400);
+    try {
+      const dir = await opendir(this.rootDir);
+      for await (const dirent of dir) {
+        const filePath = path.join(this.rootDir, dirent.name);
+        const fileStats = await stat(filePath);
+        const ageInSeconds = Math.floor(
+          (Date.now() - fileStats.mtimeMs) / 1000
+        );
+        const ageInDays = Math.floor(ageInSeconds / 86400);
 
-      const fsEvent: FSEvent = {
-        name: dirent.name,
-        path: filePath,
-        type: dirent.isDirectory() ? FS_TYPE.DIR : FS_TYPE.FILE,
-        size: fileStats.size || 0,
-        ageInDays,
-        ageInSeconds,
-      };
+        const fsEvent: FSEvent = {
+          name: dirent.name,
+          path: filePath,
+          type: dirent.isDirectory() ? FS_TYPE.DIR : FS_TYPE.FILE,
+          size: fileStats.size || 0,
+          ageInDays,
+          ageInSeconds,
+        };
 
-      if (dirent.isDirectory()) {
-        // emit dir stats and call recursevily to scan
-        // files in the directory founf
-        this.emit(FS_TYPE.DIR, fsEvent);
-        await this.scan(filePath);
-      } else {
-        //emit file stats
-        this.emit(FS_TYPE.FILE, fsEvent);
+        if (dirent.isDirectory()) {
+          // emit dir stats and call recursevily to scan
+          // files in the directory founf
+          this.emit(FS_TYPE.DIR, fsEvent);
+          await this.scan(filePath);
+        } else {
+          //emit file stats
+          this.emit(FS_TYPE.FILE, fsEvent);
+        }
       }
+    } catch (err: any) {
+      this.emit(FS_TYPE.ERROR, err.message);
     }
   }
 }
-
-// async function main() {
-//   const f = new FileSysScanHandler(path.join(__dirname, "..", ".."));
-//   await f.scan();
-// }
-
-// main()
-//   .then((suc) => console.log(suc))
-//   .catch((err) => console.log(err));
